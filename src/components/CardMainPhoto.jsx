d9e5d5cf84bf899824dc1e3480283fbc1a4310e0
marco-dias-windows11 <marcodias.bko@gmail.com>
2023-11-30 20:57:49 -0300

    image set

--- src/components/CardMainPhoto.jsx
+++ src/components/CardMainPhoto.jsx
@@ -2,6 +2,7 @@ export function CardMainPhoto() {
     return (
         <>
             <div className="main-photo">
+                <img src="../../public\image-main1.jpg" alt="head-phone-studio" />
                 <h3 className="image-text">O segredo não é como fazer,
                     e sim o porquê fazer.</h3>
             </div>
--- src/components/CardServices.jsx
+++ src/components/CardServices.jsx
@@ -4,11 +4,11 @@ export function CardServices() {
             <div className="servicecardcontainer">
                 <h5>Escolha um serviço</h5>
                 <ul>
-                    <li className="servicesli">Gravação de áudio</li>
-                    <li className="servicesli">Arte da capa</li>
-                    <li className="servicesli">Mixagem e Masterização</li>
-                    <li className="servicesli">Design para o marketing digital</li>
-                    <li className="servicesli">Foto e edição</li>
+                    <li className="servicesli">Gravação de áudio<i>&#x26AC;</i></li>
+                    <li className="servicesli">Arte da capa<i>&#x26AC;</i></li>
+                    <li className="servicesli">Mixagem e Masterização<i>&#x26AC;</i></li>
+                    <li className="servicesli">Design para marketing<i>&#x26AC;</i></li>
+                    <li className="servicesli">Foto e edição<i>&#x26AC;</i></li>
                 </ul>
             </div>
         </>
